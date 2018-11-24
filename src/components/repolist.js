fe68fab69329388a03242f10b2fcdd6da186b457
Kevin Shionen Gomez Veliz <masterk@MacBook-Pro-MasterK.local>
2018-11-24 20:15:00 -0300

    modal working

--- src/components/repolist.js
+++ src/components/repolist.js
@@ -1,44 +1,118 @@
-import React, { Component } from 'react';
-import { Container, Content, Header, Left, Right, Body, Title, Text, Button, Spinner, List, ListItem, Icon } from 'native-base';
-import { Actions } from 'react-native-router-flux';
+import React, { Component } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
+import Modal from "react-native-modal";
 import MapView from 'react-native-maps';
 
-class RepoList extends Component {
-  componentWillMount() {
-  }
+export default class RepoList extends Component {
+  state = {
+    visibleModal: true
+  };
+
+  handleOnScroll = event => {
+    this.setState({
+      scrollOffset: event.nativeEvent.contentOffset.y
+    });
+  };
+
+  handleScrollTo = p => {
+    if (this.scrollViewRef) {
+      this.scrollViewRef.scrollTo(p);
+    }
+  };
+
   render() {
     return (
-      <MapView
-        style={styles.map} 
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+      <View style={styles.map}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+        </MapView>
+        <Modal
+          isVisible={this.state.visibleModal}
+          onSwipe={() => this.setState({ visibleModal: null })}
+          swipeDirection="down"
+          scrollTo={this.handleScrollTo}
+          scrollOffset={this.state.scrollOffset}
+          scrollOffsetMax={400 - 300} // content height - ScrollView height
+          style={styles.bottomModal}
+        >
+          <View style={styles.scrollableModal}>
+            <ScrollView
+              ref={ref => (this.scrollViewRef = ref)}
+              onScroll={this.handleOnScroll}
+              scrollEventThrottle={16}
+            >
+              <View key={1} style={styles.scrollableModalContent1}>
+                <Text>Scroll me up</Text>
+              </View>
+              <View key={2} style={styles.scrollableModalContent1}>
+                <Text>Scroll me up</Text>
+              </View>
+            </ScrollView>
+          </View>
+        </Modal>
+      </View>
     );
   }
 }
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#fff'
-  },
-  text: {
-      fontSize: 30,
-      fontWeight: '700',
-      color: '#59656C',
-      marginBottom: 10,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
   },
   map: {
-      width: '100%',
-      height: '100%',
-      flex: 1
+    width: '100%',
+    height: '100%',
+    flex: 1
+  },
+  button: {
+    backgroundColor: "lightblue",
+    padding: 12,
+    margin: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    borderColor: "rgba(0, 0, 0, 0.1)"
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 22,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    borderColor: "rgba(0, 0, 0, 0.1)"
+  },
+  bottomModal: {
+    justifyContent: "flex-end",
+    margin: 0
+  },
+  scrollableModal: {
+    height: 300
+  },
+  scrollableModalContent1: {
+    height: 200,
+    backgroundColor: "orange",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  scrollableModalContent2: {
+    height: 200,
+    backgroundColor: "lightgreen",
+    alignItems: "center",
+    justifyContent: "center"
   }
-};
-
-export default RepoList;
+});
